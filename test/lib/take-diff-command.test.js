fbf6d6499fead5c6e91584bddadfadc182e8ea65
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-09 00:35:24 +1000

    Open two texts in two editors and compare

---
+++ test/lib/take-diff-command.test.js
@@ -0,0 +1,12 @@
+
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const TakdDiffCommand = require('../../lib/take-diff-command');
+
+suite('TakdDiffCommand', () => {
+
+    test('compares two texts', () => {
+    });
+
+});
