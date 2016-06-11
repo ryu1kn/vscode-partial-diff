fcfb44e4f20ab010d9d690eda504bc3727692f2e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-06-11 11:54:55 +1000

    Fix that it didn't work when try to mark the 2nd text before the 1st

--- test/lib/app.test.js
+++ test/lib/app.test.js
@@ -9,32 +9,59 @@ suite('App', () => {
             const vscode = fakeVSCode();
             const tempFileWriter = {write: sinon.spy()};
             const app = new App({vscode, tempFileWriter});
-            app.handleMarkSection1Command();
-            expect(tempFileWriter.write).to.have.been.calledWith('SELECTED_TEXT');
+            return app.handleMarkSection1Command().then(() => {
+                expect(tempFileWriter.write).to.have.been.calledWith('SELECTED_TEXT');
+            });
         });
 
         test('it prints callstack if error occurred', () => {
-            const vscode = {}
+            const vscode = {};
             const logger = {error: sinon.spy()};
             const app = new App({vscode, logger});
-            app.handleMarkSection1Command();
-            expect(logger.error.args[0][0].slice(0, 63)).to.eql(
-                "TypeError: Cannot read property 'activeTextEditor' of undefined"
-            );
+            return app.handleMarkSection1Command().then(() => {
+                expect(logger.error.args[0][0].slice(0, 63)).to.eql(
+                    "TypeError: Cannot read property 'activeTextEditor' of undefined"
+                );
+            });
+        });
+
+        test('it prints callstack if saving to a file failed', () => {
+            const vscode = fakeVSCode();
+            const logger = {error: sinon.spy()};
+            const tempFileWriter = {write: sinon.stub().returns(Promise.reject(new Error('WRITE_ERROR')))};
+            const app = new App({vscode, logger, tempFileWriter});
+            return app.handleMarkSection1Command().then(() => {
+                expect(logger.error.args[0][0].slice(0, 18)).to.eql('Error: WRITE_ERROR');
+            });
         });
     });
 
     suite('#handleMarkSection2AndTakeDiffCommand', () => {
 
-        test('it saves selected text', () => {
+        test('it saves selected text and takes a diff of 2 texts', () => {
             const vscode = fakeVSCode();
-            const logger = console;
-            const tempFileWriter = {write: sinon.stub().returns(Promise.resolve('FILE_PATH'))};
+            const tempFileWriter = fakeTempFileWriter('FILE_PATH_1', 'FILE_PATH_2');
             const diffPresenter = {takeDiff: sinon.spy()};
-            const app = new App({vscode, logger, diffPresenter, tempFileWriter});
-            return app.handleMarkSection2AndTakeDiffCommand().then(() => {
-                expect(diffPresenter.takeDiff).to.have.been.calledWith(null, 'FILE_PATH');
-            });
+            const app = new App({vscode, diffPresenter, tempFileWriter});
+            return Promise.resolve()
+                .then(app.handleMarkSection1Command.bind(app))
+                .then(app.handleMarkSection2AndTakeDiffCommand.bind(app))
+                .then(() => {
+                    expect(diffPresenter.takeDiff).to.have.been.calledWith('FILE_PATH_1', 'FILE_PATH_2');
+                });
+        });
+
+        test('it does nothing if text1 is not yet selected', () => {
+            const vscode = fakeVSCode();
+            const tempFileWriter = fakeTempFileWriter(null, 'FILE_PATH_2');
+            const diffPresenter = {takeDiff: sinon.spy()};
+            const app = new App({vscode, diffPresenter, tempFileWriter});
+            return Promise.resolve()
+                .then(app.handleMarkSection1Command.bind(app))
+                .then(app.handleMarkSection2AndTakeDiffCommand.bind(app))
+                .then(() => {
+                    expect(diffPresenter.takeDiff).to.have.been.not.called;
+                });
         });
 
         test('it prints callstack if error occurred', () => {
@@ -57,5 +84,12 @@ suite('App', () => {
             }
         };
         return {window};
+    }
+
+    function fakeTempFileWriter(filePath1, filePath2) {
+        const stub = sinon.stub();
+        stub.onCall(0).returns(Promise.resolve(filePath1));
+        stub.onCall(1).returns(Promise.resolve(filePath2));
+        return {write: stub};
     }
 });
