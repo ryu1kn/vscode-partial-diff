d788fa9cba77bcf865f41a07360951bd2bbb994d
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-11 23:06:18 +1000

    Use "N/A" as a file name if no text is selected yet
    + some refactoring

--- test/lib/diff-presenter.test.js
+++ test/lib/diff-presenter.test.js
@@ -5,10 +5,19 @@ suite('DiffPresenter', () => {
 
     test('it compares two files', () => {
         const commands = fakeCommands();
-        const diffPresenter = new DiffPresenter({commands});
-        return diffPresenter.takeDiff('TITLE', 'URI_INSTANCE_1', 'URI_INSTANCE_2').then(() => {
+        const textRegistry = {
+            get: stubReturns([
+                {fileName: 'FILE_NAME_1'},
+                {fileName: 'FILE_NAME_2'}
+            ])
+        };
+        const textResourceUtil = {
+            getUri: stubReturns(['URI_INSTANCE_1', 'URI_INSTANCE_2'])
+        };
+        const diffPresenter = new DiffPresenter({commands, textRegistry, textResourceUtil});
+        return diffPresenter.takeDiff().then(() => {
             expect(commands.executeCommand).to.have.been.calledWith(
-                'vscode.diff', 'URI_INSTANCE_1', 'URI_INSTANCE_2', 'TITLE'
+                'vscode.diff', 'URI_INSTANCE_1', 'URI_INSTANCE_2', 'FILE_NAME_1 \u2194 FILE_NAME_2'
             );
         });
     });
@@ -16,4 +25,12 @@ suite('DiffPresenter', () => {
     function fakeCommands() {
         return {executeCommand: sinon.stub().returns(Promise.resolve())};
     }
+
+    function stubReturns(returnValues) {
+        return returnValues.reduce((stub, returnValue, index) => {
+            stub.onCall(index).returns(returnValue);
+            return stub;
+        }, sinon.stub());
+    }
+
 });
