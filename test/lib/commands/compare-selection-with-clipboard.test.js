489451887386792a61ec0655f0eede35540a7b72
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-05-14 11:25:03 +1000

    Wrote tests for compare text with clipboard functionality

--- test/lib/commands/compare-selection-with-clipboard.test.js
+++ test/lib/commands/compare-selection-with-clipboard.test.js
@@ -1,2 +1,55 @@
 
-require('../../../lib/commands/compare-selection-with-clipboard');
+const CompareSelectionWithClipboardCommand = require('../../../lib/commands/compare-selection-with-clipboard');
+
+suite('CompareSelectionWithClipboardCommand', () => {
+
+    const editor = {
+        document: {
+            fileName: 'FILENAME'
+        }
+    };
+
+    test('it compares selected text with clipboard text', () => {
+        const copyPaste = {
+            paste: callback => { callback(null, 'CLIPBOARD_TEXT'); }
+        };
+        const editorLineRangeExtractor = {extract: stubWithArgs([editor], 'SELECTED_RANGE')};
+        const editorTextExtractor = {extract: stubWithArgs([editor], 'SELECTED_TEXT')};
+        const textRegistry = {set: sinon.spy()};
+        const diffPresenter = {takeDiff: sinon.spy()};
+        const command = new CompareSelectionWithClipboardCommand({
+            copyPaste,
+            diffPresenter,
+            editorLineRangeExtractor,
+            editorTextExtractor,
+            textRegistry
+        });
+        return command.execute(editor).then(() => {
+            expect(textRegistry.set).to.have.been.calledWith(
+                'clipboard',
+                {
+                    text: 'CLIPBOARD_TEXT',
+                    fileName: 'Clipboard'
+                }
+            );
+            expect(textRegistry.set).to.have.been.calledWith(
+                'reg2',
+                {
+                    text: 'SELECTED_TEXT',
+                    fileName: 'FILENAME',
+                    lineRange: 'SELECTED_RANGE'
+                }
+            );
+            expect(diffPresenter.takeDiff).to.have.been.calledWith('clipboard', 'reg2');
+        });
+    });
+
+    test('it prints callstack if error occurred', () => {
+        const logger = {error: sinon.spy()};
+        const command = new CompareSelectionWithClipboardCommand({logger});
+        return command.execute(editor).then(() => {
+            expect(logger.error).to.have.been.called;
+        });
+    });
+
+});
