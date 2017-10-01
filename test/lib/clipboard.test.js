a6e5ecf3dd214d03e95487b184199b810caeb572
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-01 23:45:47 +1100

    refs #12 Fixed the issue on compare clipboard on Windows
    Remove redundant CR char from clipboard contents on Windows

--- test/lib/clipboard.test.js
+++ test/lib/clipboard.test.js
@@ -4,10 +4,19 @@ const Clipboard = require('../../lib/clipboard');
 suite('Clipboard', () => {
 
     test('it reads text from clipboard', () => {
-        const clipboardy = {read: () => Promise.resolve('CLIPBOARD_TEXT')};
+        const clipboardy = {read: () => Promise.resolve('LINE1\r\r\nLINE2\r\r\n')};
         const clipboard = new Clipboard({clipboardy});
         return clipboard.read().then(text => {
-            expect(text).to.eql('CLIPBOARD_TEXT');
+            expect(text).to.eql('LINE1\r\r\nLINE2\r\r\n');
+        });
+    });
+
+    test('it replaces `\\r\\r\\n` to `\\r\\n` on Windows', () => {
+        const clipboardy = {read: () => Promise.resolve('LINE1\r\r\nLINE2\r\r\n')};
+        const platform = 'win32';
+        const clipboard = new Clipboard({clipboardy, platform});
+        return clipboard.read().then(text => {
+            expect(text).to.eql('LINE1\r\nLINE2\r\n');
         });
     });
 
