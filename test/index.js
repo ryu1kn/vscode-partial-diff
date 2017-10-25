434cedfe161fbff696c8dc48aadd94946c3cd71e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-25 23:11:08 +1100

    Fix lint errors

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-//
+/* eslint-disable capitalized-comments */
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
 // This file is providing the test runner to use when running extension tests.
