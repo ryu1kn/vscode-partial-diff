af058187cd4af0f65754f3ab1dde9a17a3c5c572
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-26 23:01:06 +1100

    refs #16 Extract texts from multi cursors

--- test/lib/editor-text-extractor.test.js
+++ test/lib/editor-text-extractor.test.js
@@ -5,25 +5,42 @@ suite('EditorTextExtractor', () => {
 
     test('it extracts a text from editor for comparison', () => {
         const extractor = new EditorTextExtractor();
-        const editor = fakeEditor('SELECTED_TEXT');
+        const editor = fakeEditor(['SELECTED_TEXT']);
         expect(extractor.extract(editor)).to.eql('SELECTED_TEXT');
     });
 
     test('it extracts a whole text if no text is currently selected', () => {
         const extractor = new EditorTextExtractor();
-        const editor = fakeEditor('');
-        expect(extractor.extract(editor)).to.eql('ENTIRE TEXT ');
+        const editor = fakeEditor(['']);
+        expect(extractor.extract(editor)).to.eql('ENTIRE TEXT');
     });
 
-    function fakeEditor(selectedText) {
+    test('it extracts texts selected by all cursors and join them with newline character', () => {
+        const extractor = new EditorTextExtractor();
+        const editor = fakeEditor(['SELECTED_TEXT_1', 'SELECTED_TEXT_2']);
+        expect(extractor.extract(editor)).to.eql('SELECTED_TEXT_1\nSELECTED_TEXT_2');
+    });
+
+    test('it ignores cursor that is not selecting text if others are selecting one', () => {
+        const extractor = new EditorTextExtractor();
+        const editor = fakeEditor(['', 'SELECTED_TEXT_1', '', 'SELECTED_TEXT_2']);
+        expect(extractor.extract(editor)).to.eql('SELECTED_TEXT_1\nSELECTED_TEXT_2');
+    });
+
+    test('it extracts the whole text if no cursors are selecting text', () => {
+        const extractor = new EditorTextExtractor();
+        const editor = fakeEditor(['', '', '']);
+        expect(extractor.extract(editor)).to.eql('ENTIRE TEXT');
+    });
+
+    function fakeEditor(selectedTexts) {
+        const selections = selectedTexts.map(text => ({text, isEmpty: !text}));
         return {
-            selection: {
-                text: selectedText,
-                isEmpty: !selectedText
-            },
+            selections,
+            selection: selections[0],
             document: {
-                _entireText: `ENTIRE TEXT ${selectedText}`,
-                getText: function (selection) {
+                _entireText: 'ENTIRE TEXT',
+                getText(selection) {
                     return selection ? selection.text : this._entireText;
                 }
             }
