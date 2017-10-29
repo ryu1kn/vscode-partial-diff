8374bbb8aac116b673d82bda3e0777802d34a989
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-29 23:53:12 +1100

    ref #15 Allow transforming texts before comparison
    User can preprocess texts before comparing them

--- test/lib/content-provider.test.js
+++ test/lib/content-provider.test.js
@@ -4,20 +4,61 @@ const ContentProvider = require('../../lib/content-provider');
 suite('ContentProvider', () => {
 
     test('it extracts text key from the given uri and uses it to retrieve text', () => {
-        const selectionInfoRegistry = {
-            get: key => {return {text: `TEXT_${key}`};}
-        };
-        const textResourceUtil = {getTextKey: uri => uri.replace('URI_', '')};
-        const contentProvider = new ContentProvider({selectionInfoRegistry, textResourceUtil});
-        const content = contentProvider.provideTextDocumentContent('URI_1');
+        const content = retrieveEditorContent({});
         expect(content).to.eql('TEXT_1');
     });
 
     test('it returns an empty string if a text is not yet selected', () => {
         const selectionInfoRegistry = {get: () => {}};
-        const textResourceUtil = {getTextKey: uri => uri.replace('URI_', '')};
-        const contentProvider = new ContentProvider({selectionInfoRegistry, textResourceUtil});
-        const content = contentProvider.provideTextDocumentContent('URI_1');
+        const content = retrieveEditorContent({selectionInfoRegistry});
         expect(content).to.eql('');
     });
+
+    test('it uses a user defined rule to preprocess text to compare', () => {
+        const preComparisonTextProcessRules = [{rule: '({text}) => text.toLowerCase()'}];
+        const content = retrieveEditorContent({preComparisonTextProcessRules});
+        expect(content).to.eql('text_1');
+    });
+
+    test('it applies all given rules to preprocess text', () => {
+        const preComparisonTextProcessRules = [
+            {rule: '({text}) => text.toLowerCase()'},
+            {rule: '({text}) => text.split("").join("-")'}
+        ];
+        const content = retrieveEditorContent({preComparisonTextProcessRules});
+        expect(content).to.eql('t-e-x-t-_-1');
+    });
+
+    test('it throws an error if a preprocess rule is not a function', () => {
+        const preComparisonTextProcessRules = [
+            {rule: '"NOT A FUNCTION JUST A STRING"'}
+        ];
+        expect(() => {
+            retrieveEditorContent({preComparisonTextProcessRules});
+        }).to.throw('Rule must be evaluated to a function');
+    });
+
+    test('it treats preprocess result as a text', () => {
+        const preComparisonTextProcessRules = [
+            {rule: '() => 1'},
+            {rule: '({text}) => typeof text'}
+        ];
+        const content = retrieveEditorContent({preComparisonTextProcessRules});
+        expect(content).to.eql('string');
+    });
+
+    function retrieveEditorContent({selectionInfoRegistry, preComparisonTextProcessRules}) {
+        const defaultSelectionInfoRegistry = {get: key => ({text: `TEXT_${key}`})};
+        const textResourceUtil = {getTextKey: uri => uri.replace('URI_', '')};
+        const configStore = {
+            get: key => key === 'preComparisonTextProcessRules' && (preComparisonTextProcessRules || [])
+        };
+        const contentProvider = new ContentProvider({
+            selectionInfoRegistry: selectionInfoRegistry || defaultSelectionInfoRegistry,
+            textResourceUtil,
+            configStore
+        });
+        return contentProvider.provideTextDocumentContent('URI_1');
+    }
+
 });
