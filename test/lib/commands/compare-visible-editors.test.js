3ac6bd6861907908807b53aa602a0a9bfed91ac9
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-13 22:50:31 +1000

    ref #11 Execute CompareVisibleEditors command only when 2 tabs are open

--- test/lib/commands/compare-visible-editors.test.js
+++ test/lib/commands/compare-visible-editors.test.js
@@ -2,26 +2,35 @@ const CompareVisibleEditorsCommand = require('../../../lib/commands/compare-visi
 const td = require('testdouble')
 
 suite('CompareVisibleEditorsCommand', () => {
-  const editorWindow = {
-    visibleTextEditors: ['EDITOR_1', 'EDITOR_2']
-  }
-  const selectionInfoBuilder = td.object(['extract'])
-  td.when(selectionInfoBuilder.extract('EDITOR_1')).thenReturn('TEXT_INFO1')
-  td.when(selectionInfoBuilder.extract('EDITOR_2')).thenReturn('TEXT_INFO2')
-  const selectionInfoRegistry = td.object(['set'])
-  const diffPresenter = td.object(['takeDiff'])
-  const command = new CompareVisibleEditorsCommand({
-    editorWindow,
-    diffPresenter,
-    selectionInfoBuilder,
-    selectionInfoRegistry
-  })
-
   test('it compares 2 visible editors', async () => {
+    const {command, deps} = createCommand(['EDITOR_1', 'EDITOR_2'])
     await command.execute()
 
-    td.verify(selectionInfoRegistry.set('visible1', 'TEXT_INFO1'))
-    td.verify(selectionInfoRegistry.set('visible2', 'TEXT_INFO2'))
-    td.verify(diffPresenter.takeDiff('visible1', 'visible2'))
+    td.verify(deps.selectionInfoRegistry.set('visible1', 'TEXT_INFO1'))
+    td.verify(deps.selectionInfoRegistry.set('visible2', 'TEXT_INFO2'))
+    td.verify(deps.diffPresenter.takeDiff('visible1', 'visible2'))
   })
+
+  test('it tells you that it needs 2 visible editors', async () => {
+    const {command, deps} = createCommand(['EDITOR_1'])
+    await command.execute()
+
+    td.verify(deps.messageBar.showInfo('Please first open 2 documents to compare.'))
+  })
+
+  function createCommand (visibleTextEditors) {
+    const selectionInfoBuilder = td.object(['extract'])
+    td.when(selectionInfoBuilder.extract('EDITOR_1')).thenReturn('TEXT_INFO1')
+    td.when(selectionInfoBuilder.extract('EDITOR_2')).thenReturn('TEXT_INFO2')
+
+    const dependencies = {
+      editorWindow: { visibleTextEditors },
+      diffPresenter: td.object(['takeDiff']),
+      messageBar: td.object(['showInfo']),
+      selectionInfoBuilder,
+      selectionInfoRegistry: td.object(['set'])
+    }
+    const command = new CompareVisibleEditorsCommand(dependencies)
+    return {command, deps: dependencies}
+  }
 })
