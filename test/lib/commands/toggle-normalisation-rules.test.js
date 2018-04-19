b5dd7d063c5f7381b390edfd420fe510e8e2dacd
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-19 19:52:53 +1000

    ref #18 Just show message if no preComparisonTextNormalisationRules are given

--- test/lib/commands/toggle-normalisation-rules.test.js
+++ test/lib/commands/toggle-normalisation-rules.test.js
@@ -3,21 +3,42 @@ const td = require('testdouble')
 const ToggleNormalisationRulesCommand = require('../../../lib/commands/toggle-normalisation-rules')
 
 suite('ToggleNormalisationRulesCommand', () => {
-  const RULES = 'RULES'
-  const normalisationRuleStore = td.object('getAllRules', 'specifyActiveRules')
-  td.when(normalisationRuleStore.getAllRules()).thenReturn(RULES)
-  const normalisationRulePicker = td.object('show')
-  td
-    .when(normalisationRulePicker.show(RULES))
-    .thenResolve('ACTIVE_RULE_INDICES')
-  const command = new ToggleNormalisationRulesCommand({
-    normalisationRulePicker,
-    normalisationRuleStore
-  })
-
   test('it updates the status of normalisation rules as user specified', async () => {
+    const { command, deps } = createCommand({ rules: ['RULE'] })
     await command.execute()
 
-    td.verify(normalisationRuleStore.specifyActiveRules('ACTIVE_RULE_INDICES'))
+    td.verify(
+      deps.normalisationRuleStore.specifyActiveRules('ACTIVE_RULE_INDICES')
+    )
   })
+
+  test('it just shows message if no rules are defined', async () => {
+    const { command, deps } = createCommand({ rules: [] })
+    await command.execute()
+
+    td.verify(
+      deps.messageBar.showInfo(
+        'Please set `partialDiff.preComparisonTextNormalizationRules` first'
+      )
+    )
+  })
+
+  function createCommand ({ rules }) {
+    const normalisationRuleStore = td.object(
+      'getAllRules',
+      'specifyActiveRules'
+    )
+    td.when(normalisationRuleStore.getAllRules()).thenReturn(rules)
+    const normalisationRulePicker = td.object('show')
+    td
+      .when(normalisationRulePicker.show(rules))
+      .thenResolve('ACTIVE_RULE_INDICES')
+    const deps = {
+      messageBar: td.object('showInfo'),
+      normalisationRulePicker,
+      normalisationRuleStore
+    }
+    const command = new ToggleNormalisationRulesCommand(deps)
+    return { command, deps }
+  }
 })
