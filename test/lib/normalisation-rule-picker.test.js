6b9075cba253e131b06f1fcd31984483a2e294af
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-19 19:24:07 +1000

    ref #18 Does nothing if user dismissed the rule selection menu

--- test/lib/normalisation-rule-picker.test.js
+++ test/lib/normalisation-rule-picker.test.js
@@ -17,12 +17,35 @@ suite('NormalisationRulePicker', () => {
       )
     )
     .thenResolve([{ label: 'RULE_NAME_2', picked: true, ruleIndex: 1 }])
+  td
+    .when(
+      vscWindow.showQuickPick(
+        [
+          { label: 'RULE_NAME_3', picked: false, ruleIndex: 0 },
+          { label: 'RULE_NAME_4', picked: true, ruleIndex: 1 }
+        ],
+        {
+          canPickMany: true
+        }
+      )
+    )
+    .thenResolve()
+
   const rulePicker = new NormalisationRulePicker({ vscWindow })
 
   test('it returns the index of active rules that user chose', async () => {
     const newStatus = await rulePicker.show([
       { name: 'RULE_NAME_1', active: true },
       { name: 'RULE_NAME_2', active: false }
+    ])
+
+    expect(newStatus).to.eql([1])
+  })
+
+  test('it returns the indices of all active rules if user dismissed the selection popup', async () => {
+    const newStatus = await rulePicker.show([
+      { name: 'RULE_NAME_3', active: false },
+      { name: 'RULE_NAME_4', active: true }
     ])
 
     expect(newStatus).to.eql([1])
