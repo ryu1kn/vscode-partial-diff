4d6a6a09409c959a1c82c4ca0a5f07f47f4fa8b3
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-19 19:33:10 +1000

    ref #18 Tell a user if a "name" is not given to a rule

--- test/lib/normalisation-rule-picker.test.js
+++ test/lib/normalisation-rule-picker.test.js
@@ -30,24 +30,42 @@ suite('NormalisationRulePicker', () => {
       )
     )
     .thenResolve()
+  td
+    .when(
+      vscWindow.showQuickPick(
+        [
+          { label: '(no "name" set for this rule)', picked: true, ruleIndex: 0 }
+        ],
+        {
+          canPickMany: true
+        }
+      )
+    )
+    .thenResolve([])
 
   const rulePicker = new NormalisationRulePicker({ vscWindow })
 
   test('it returns the index of active rules that user chose', async () => {
-    const newStatus = await rulePicker.show([
+    const activeRuleIndices = await rulePicker.show([
       { name: 'RULE_NAME_1', active: true },
       { name: 'RULE_NAME_2', active: false }
     ])
 
-    expect(newStatus).to.eql([1])
+    expect(activeRuleIndices).to.eql([1])
   })
 
   test('it returns the indices of all active rules if user dismissed the selection popup', async () => {
-    const newStatus = await rulePicker.show([
+    const activeRuleIndices = await rulePicker.show([
       { name: 'RULE_NAME_3', active: false },
       { name: 'RULE_NAME_4', active: true }
     ])
 
-    expect(newStatus).to.eql([1])
+    expect(activeRuleIndices).to.eql([1])
+  })
+
+  test('it shows a note to tell the user that no name is given for the rule', async () => {
+    const activeRuleIndices = await rulePicker.show([{ active: true }])
+
+    expect(activeRuleIndices).to.eql([])
   })
 })
