dd05b2840de88503d03953a9205e682422f7ee67
Adam Smith <foo@bar.com>
2020-08-09 11:54:06 +0100

    Fix types

--- src/lib/adaptors/git.ts
+++ src/lib/adaptors/git.ts
@@ -1,22 +1,29 @@
 import * as vscode from 'vscode';
+import { Repository, Ref, Branch } from '../types/git.d'
 
 export default class GitAdapter {
-    repo: any;
+    repo: Repository;
     constructor(private readonly extensions: typeof vscode.extensions) {
         const ext = this.extensions.getExtension('vscode.git')?.exports;
         const api = ext.getAPI(1);
-		this.repo = api.repositories[0];
+        this.repo = api.repositories[0];
     }
 
-    show(branchName: string, fileDir: string): string {
-       return this.repo.show(branchName, fileDir);
+    isGitRepo(): boolean {
+        return !!this.repo;
     }
 
-    async allBranches<T>(): Promise<T> {
-        return await this.repo.getBranches('');
+    async show(branchName: string, fileDir: string): Promise<string> {
+        return this.repo.show(branchName, fileDir);
     }
 
-    async getBranch<T>(name: String): Promise<T> {
+    async allBranches(): Promise<Ref[]> {
+        return await this.repo.getBranches({
+            pattern: ''
+        });
+    }
+
+    async getBranch(name: string): Promise<Branch> {
         return await this.repo.getBranch(name);
     }
 }
--- src/lib/commands/compare-with-git-branch.ts
+++ src/lib/commands/compare-with-git-branch.ts
@@ -2,8 +2,17 @@ import DiffPresenter from '../diff-presenter';
 import SelectionInfoRegistry from '../selection-info-registry';
 import {TextKey} from '../const';
 import {Command} from './command';
+import * as path from 'path';
 import WindowAdaptor from '../adaptors/window';
+import { Ref } from '../types/git.d'
 import GitAdaptor from '../adaptors/git';
+import { QuickPickItem } from 'vscode';
+
+interface FileDetails {
+    uri: string;
+    name: string;
+    text: string,
+}
 
 export default class CompareWithGitBranchCommand implements Command {
     constructor(private readonly diffPresenter: DiffPresenter,
@@ -11,31 +20,61 @@ export default class CompareWithGitBranchCommand implements Command {
                 private readonly gitAdaptor: GitAdaptor,
                 private readonly windowAdaptor: WindowAdaptor) {}
 
-    async execute() {
-        const branches: [] = await this.gitAdaptor.allBranches();
-        const branchNames = branches.map((i:any) => i.name);
-
-        const branch = await this.windowAdaptor.showQuickPick(branchNames, false);
+    private getActiveFileDetails(): FileDetails {
         const document = this.windowAdaptor.activeTextEditor.document;
-        const localFileUri = document.fileName; 
-        const localText = document.getText();
+        const uri = document.fileName; 
+        const text = document.getText();
 
-        const branchName = typeof branch === 'string' ? branch : '';
-        const branchText = await this.gitAdaptor.show(branchName, localFileUri);
+        const name = path.parse(uri).base;
+
+        return {
+            uri,
+            name,
+            text
+        }
+    }
+
+    private toQuickPickItems(list: string[]): QuickPickItem[] {
+        return list.map((name, i)=> ({
+            label: name,
+            picked: false,
+            ruleIndex: i,
+            description: ''  
+        }));
+    }
+
+    private async getSelectedBranchName(branchNames: string[]): Promise<string> {
+        const branch: QuickPickItem = await <any>this.windowAdaptor.showQuickPick(this.toQuickPickItems(branchNames), false);
+        return branch.label;
+    }
+
+    private async getLocalBranchNames(): Promise<string[]> {
+        if (!this.gitAdaptor.isGitRepo()) {
+            this.windowAdaptor.showInformationMessage("Git repo not found!");
+        }
+        const branches: Ref[] = await this.gitAdaptor.allBranches();
+        return branches.map((i:Ref) => i.name || '');
+    }
+
+    async execute() {
+        const branchNames = await this.getLocalBranchNames();
+        const branchName = await this.getSelectedBranchName(branchNames);
+        const activeFile = await this.getActiveFileDetails();
+        const branchText = await this.gitAdaptor.show(branchName, activeFile.uri);
 
         this.selectionInfoRegistry.set(TextKey.VISIBLE_EDITOR1, {
-            text: localText, 
-            fileName: localFileUri, 
+            text: activeFile.text, 
+            fileName: `${activeFile.name} (local)`, 
             lineRanges: []
         });
 
         this.selectionInfoRegistry.set(TextKey.GIT_BRANCH, {
             text: branchText, 
-            fileName: localFileUri,
+            fileName: `${activeFile.name} (${branchName})`, 
             lineRanges: []
         });
 
         await 'HACK'; // HACK: Avoid "TextEditor has been disposed" error
-        await this.diffPresenter.takeDiff(TextKey.VISIBLE_EDITOR1, TextKey.GIT_BRANCH);
+        await this.diffPresenter.takeDiff(TextKey.GIT_BRANCH, TextKey.VISIBLE_EDITOR1);
     }
 }
