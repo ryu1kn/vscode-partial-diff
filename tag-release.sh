c5283989961b48e31df879ba11e5db40604d2f06
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-04-16 22:49:59 +1000

    Simplify tagging script

--- tag-release.sh
+++ tag-release.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-RELEASE_VER=$(node -p 'JSON.parse(require("fs").readFileSync("./package.json", "utf8")).version')
+RELEASE_VER="$(node -p 'require("./package.json").version')"
 
-GIT_TAG_NAME=v$RELEASE_VER
-git tag -a $GIT_TAG_NAME -m $GIT_TAG_NAME
-git push origin $GIT_TAG_NAME
-
+GIT_TAG_NAME="v$RELEASE_VER"
+git tag -a "$GIT_TAG_NAME" -m "$GIT_TAG_NAME"
+git push origin "$GIT_TAG_NAME"
